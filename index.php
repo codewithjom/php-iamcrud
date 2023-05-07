a93e2a1a384c9ca9b6914ca38b7f6d928ed3a40d
Jom Dollesin <codewithjom@gmail.com>
2023-05-07 14:42:49 +0800

    feat: READ.

--- index.php
+++ index.php
@@ -2,42 +2,44 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>IAMCRUD</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="./src/output.css" rel="stylesheet" />
-  </head>
-  <body>
-    <section class="antialiased bg-gray-100 text-gray-600 h-screen px-4" x-data="app">
-      <div class="flex flex-col justify-center h-full">
-        <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
-          <header class="px-5 py-4 border-b border-gray-100">
-              <div class="font-semibold text-gray-800">Fruits</div>
-          </header>
-          <div class="overflow-x-auto p-3">
-            <table class="table-auto w-full">
-              <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
-                <tr>
-                  <th></th>
-                  <th class="p-2">
-                    <div class="font-semibold text-left">Fruit Name</div>
-                  </th>
-                  <th class="p-2">
-                    <div class="font-semibold text-left">Quantity</div>
-                  </th>
-                  <th class="p-2">
-                    <div class="font-semibold text-left">Registered</div>
-                  </th>
-                  <th class="p-2">
-                    <div class="font-semibold text-left">Updated</div>
-                  </th>
-                  <th></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <?php $getFruit = selectedFruits();
-while($fruit=$getFruit->fetch(PDO::FETCH_ASSOC)) { ?>
+
+<head>
+  <title>IAMCRUD</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="./src/output.css" rel="stylesheet" />
+</head>
+
+<body>
+  <section class="antialiased bg-gray-100 text-gray-600 h-screen px-4" x-data="app">
+    <div class="flex flex-col justify-center h-full">
+      <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+        <header class="px-5 py-4 border-b border-gray-100">
+          <div class="font-semibold text-gray-800">Fruits</div>
+        </header>
+        <div class="overflow-x-auto p-3">
+          <table class="table-auto w-full">
+            <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+              <tr>
+                <th></th>
+                <th class="p-2">
+                  <div class="font-semibold text-left">Fruit Name</div>
+                </th>
+                <th class="p-2">
+                  <div class="font-semibold text-left">Quantity</div>
+                </th>
+                <th class="p-2">
+                  <div class="font-semibold text-left">Registered</div>
+                </th>
+                <th class="p-2">
+                  <div class="font-semibold text-left">Updated</div>
+                </th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <?php $getFruit = selectedFruits();
+            while ($fruit = $getFruit->fetch(PDO::FETCH_ASSOC)) { ?>
               <tbody class="text-sm divide-y divide-gray-100">
                 <tr>
                   <td class="p-2">
@@ -67,15 +69,16 @@ while($fruit=$getFruit->fetch(PDO::FETCH_ASSOC)) { ?>
                   </td>
                 </tr>
               </tbody>
-              <?php } ?>
-            </table>
-          </div>
-          <div class="flex justify-end font-bold space-x-4 text-2xl border-t border-gray-100 px-5 py-4">
-              <div>Total</div>
-              <div class="text-blue-600">₱<span x-text="total.toFixed(2)"></span></div>
-          </div>
+            <?php } ?>
+          </table>
+        </div>
+        <div class="flex justify-end font-bold space-x-4 text-2xl border-t border-gray-100 px-5 py-4">
+          <div>Total</div>
+          <div class="text-blue-600">₱<span x-text="total.toFixed(2)"></span></div>
         </div>
       </div>
-    </section>
-  </body>
+    </div>
+  </section>
+</body>
+
 </html>
