2929625bd54240034e6a324e1c6de070f8d3b96c
Jom Dollesin <codewithjom@gmail.com>
2023-05-07 16:12:45 +0800

    feat: READ.

--- _functions.php
+++ _functions.php
@@ -2,16 +2,16 @@
 
 function connection()
 {
-    date_default_timezone_set('Asia/Manila');
-    $dbConnection = new PDO('mysql:dbname=php_iamcrud;host=localhost;charset=utf8mb4', 'root', '');
-    $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $dbConnection;
+  date_default_timezone_set('Asia/Manila');
+  $dbConnection = new PDO('mysql:dbname=php_iamcrud;host=localhost;charset=utf8mb4', 'root', '');
+  $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $dbConnection;
 }
 
 function selectedFruits()
 {
-    $statement=connection()->prepare("SELECT * FROM fruits Order By fruit_id ASC");
-    $statement->execute();
-    return $statement;
+  $statement = connection()->prepare("SELECT * FROM fruits Order By fruit_id ASC");
+  $statement->execute();
+  return $statement;
 }
