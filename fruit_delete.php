a12834e697de4bb3511c5dfabb4d56d640459a77
Jom Dollesin <codewithjom@gmail.com>
2023-05-09 15:19:24 +0800

    feat: CRUD with styles.

--- fruit_delete.php
+++ fruit_delete.php
@@ -22,13 +22,30 @@ if (isset($_POST['yes'])) {
 
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>IAMCRUD</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="./src/output.css" rel="stylesheet" />
+</head>
+
 
 <body>
-  <form method='post'>
-    <input type='submit' name='yes' value='Yes'>
-    <input type='submit' name='yes' value='No'>
-  </form>
+  <section class="antialiased bg-gray-100 text-gray-600 h-screen px-4" x-data="app">
+    <div class="flex flex-col justify-center h-full">
+      <div class="w-full max-w-md mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+        <form method='post'>
+          <div class='flex m-10 justify-center space-x-4'>
+            <input type='submit' name='yes' value='Yes' class='py-2 px-8 rounded-md bg-red-500 hover:bg-red-600 text-white'>
+            <input type='submit' name='no' value='No' class='py-2 px-8 rounded-md bg-stone-300 hover:bg-stone-400 hover:text-white'>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 </body>
 
 </html>
--- fruit_update.php
+++ fruit_update.php
@@ -17,18 +17,37 @@ if (isset($_POST['fruitedit'])) {
   header("Location: index.php");
 
   return $sql;
+} else if (isset($_POST['back'])) {
+  header("Location: index.php");
 }
 
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>IAMCRUD</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="./src/output.css" rel="stylesheet" />
+</head>
 
 <body>
-  <form method='post'>
-    <input type='text' name='updatename' placeholder='Fruit name' value='<?= $row["fruit_name"] ?>'>
-    <input type='text' name='updateqty' placeholder='Quantity' value='<?= $row["fruit_qty"] ?>'>
-    <input type='submit' name='fruitedit' value='Save'>
-  </form>
+  <section class="antialiased bg-gray-100 text-gray-600 h-screen px-4" x-data="app">
+    <div class="flex flex-col justify-center h-full">
+      <div class="w-full max-w-3xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+        <form method='post'>
+          <div class='flex m-10 justify-center space-x-4'>
+            <input type='text' name='updatename' placeholder='Fruit name' value='<?= $row["fruit_name"] ?>' class='border px-2 py-1'>
+            <input type='text' name='updateqty' placeholder='Quantity' value='<?= $row["fruit_qty"] ?>' class='border px-2 py-1'>
+            <input type='submit' name='fruitedit' value='Save' class='py-2 px-8 rounded-md bg-green-500 hover:bg-green-600 text-white'>
+            <input type='submit' name='back' value='Back' class='py-2 px-8 rounded-md bg-stone-300 hover:bg-stone-400 hover:text-white'>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 </body>
 
 </html>
